9b4224a390561dd1f989216ce6320b76020a664b
Bastarus <bastarus0@gmail.com>
2020-08-08 19:56:51 +0300

    fix

--- src/blocks/modules/footer/footer.js
+++
@@ -1,6 +0,0 @@
-const selectLanguage = document.querySelector('#selectLanguage')
-const languages = document.querySelector('.languages')
-
-selectLanguage.addEventListener('click', () => {
-    languages.classList.toggle('languages_active')
-})
